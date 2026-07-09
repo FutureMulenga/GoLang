11dc2614b720aaf239002c36dae889f81fd23b21
FUTURE MULENGA <mulengafuture14@gmail.com>
2026-07-09 19:08:40 +0200

    arrays

---
+++ arrays.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+
+func arrays() {
+
+	numbers := [5]int{1, 2, 3, 4, 5} // Array of integers with a fixed size of 5``
+
+	fmt.Println("Numbers:", numbers, "Length:", len(numbers)) // Printing the array and its length using the len() function
+}
