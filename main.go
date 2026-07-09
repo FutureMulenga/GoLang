b651a495d2f81d6e54606eaa0f1104717697cde0
FUTURE MULENGA <mulengafuture14@gmail.com>
2026-07-09 15:55:38 +0200

    data types

--- main.go
+++ main.go
@@ -3,8 +3,22 @@ package main
 import "fmt"
 
 
+func dataTypes() {
+
+	var age int8 = 25 // Explicit type declaration, specifying that age is of type int
+	var uintAge uint8 = 30 // Explicit type declaration, specifying that uintAge is of type uint
+    var height float32 = 5.9 // Explicit type declaration, specifying that height is of type float32
+
+
+	fmt.Println("Age:", age)
+	fmt.Println("Unsigned Age:", uintAge)
+	fmt.Println("Height:", height)
+}
+
+
 func main() {
 	
+	dataTypes()
 	var firstName string  = "Future" // Explicit type declaration, specifying that firstName is of type string
 	var lastName  = "Mulenga" // Type inference, Go will automatically determine the type based on the value assigned
 	greeting := "Hello, my name is " + firstName + " " + lastName // Concatenation of strings using the + operator
